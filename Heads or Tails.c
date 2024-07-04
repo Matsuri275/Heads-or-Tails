c40c9ce748c520a8bb8f532451158d867158d7f7
matsunaga <matsunagaryoichiro@gmail.com>
2024-07-04 20:57:13 +0900

    HoT: initial revision

---
+++ Heads or Tails.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+char* toss_coin() {
+    return rand() % 2 == 0 ? "Heads" : "Tails";
+}
+
+int main() {
+    
+    printf("Tossing a coin...\n");
+
+    srand(time(0));
+  
+    int heads_count = 0, tails_count = 0;
+
+    for (int i = 1; i <= 3; i++) {
+        char* result = toss_coin();
+        printf("Round %d: %s\n", i, result);
+        if (result == "Heads") heads_count++;
+        else tails_count++;
+    }
+
+    printf("Heads: %d, Tails: %d\n", heads_count, tails_count);
+    
+    return 0;   
+}
